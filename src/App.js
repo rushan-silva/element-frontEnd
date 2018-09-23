92cdb75ab8d42cc32e3082baf18c2c37cac3a484
Yasas <UYasaDi@PEROOT.COM>
2018-09-23 08:41:01 +0530

    Add redux data flow files

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import SignIn from './containers/SignIn/SignIn';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from "react-redux";
+import store from "./Store";
+import Posts from './components/Chart/Posts';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Posts/>
       </div>
+      </Provider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+//import injectTapEventPlugin from 'react-tap-event-plugin';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SignIn from './containers/SignIn/SignIn';
 import SignUp from './containers/SignUp/SignUp';
 import Home from './containers/Home/Home';
+import EmotionAnalysisView from  './containers/Dashboard/EmotionView';
+
 
 const target = document.getElementById('root');
 
 ReactDOM.render(
-    <Router>
-        <div>
-            <Route exact path="/" component={SignIn} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path="/home" component={Home} />
-        </div>
-    </Router>,
+    // <Router>
+    //     <div>
+    //         {/* <Route exact path="/" component={SignIn} /> */}
+    //         <Route exact path="/" component={EmotionAnalysisView} />
+    //         <Route path="/sign-up" component={SignUp} />
+    //         <Route path="/home" component={Home} />
+    //     </div>
+    // </Router>,
+    <App/>,
     target
 );
 registerServiceWorker();
-injectTapEventPlugin();
+//injectTapEventPlugin();
